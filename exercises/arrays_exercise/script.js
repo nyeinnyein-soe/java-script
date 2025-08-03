36ca0c7b79fc8b32497a423fd979b8022ff16bdb
kaiyeolbaek <kaiyeolbaek8422@gmail.com>
2025-08-03 22:41:49 +0630

    push , pop , shift and unshif exercise

---
+++ exercises/arrays_exercise/script.js
@@ -0,0 +1,30 @@
+// 🔹 Exercise 1: Push & Pop Basics
+// You have an array of fruits:
+
+// js
+// Copy
+// Edit
+// let fruits = ["apple", "banana"];
+// Your Task:
+
+// Add "mango" to the end.
+
+// Remove the first fruit.
+
+// Add "orange" to the front.
+
+// What does the final array look like?"
+
+let fruits = ["apple", "banana"];
+console.log(fruits.push("mango"));
+console.log(fruits);
+
+fruits.shift();
+console.log(fruits);
+
+fruits.unshift("orange");
+console.log(fruits);
+
+
+
+
